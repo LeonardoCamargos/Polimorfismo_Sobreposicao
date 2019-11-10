4ba8f7ad901faba07995a66372ef973aa30214b4
Leonardo Camargos <leocamargoslemes@hotmail.com>
2019-11-10 21:22:21 -0200

    Exercicio polimorfismo de sobreposição

---
+++ AulaEncapsulamento/src/aula12/Main.java
@@ -0,0 +1,27 @@
+package aula12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Mamifero m = new Mamifero();
+		Reptil r = new Reptil();
+		Peixe p = new Peixe();
+		Ave a = new Ave();
+		
+		
+		Canguru c = new Canguru();
+		Cachorro k = new Cachorro();
+		Cobra J = new Cobra();
+		Tartaruga t = new Tartaruga();
+		Goldfish g = new Goldfish();
+		Arara arara = new Arara();
+		
+		
+		t.locomover();
+		k.enterrarOsso();
+		
+
+	}
+
+}
